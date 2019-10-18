c8e73b20236bc24993b51d3e816ecdb62b3d2cdf
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-10-17 18:05:10 -0700

    refactor: reorder form entries and remove unused method

--- src/lib/ConversionClient.ts
+++ src/lib/ConversionClient.ts
@@ -19,8 +19,8 @@ export default class ConversionClient {
 
   public async setInputFile(name: string, content: File): Promise<void> {
     const formData = new FormData()
-    formData.append('file', content)
     formData.append('name', name)
+    formData.append('file', content)
 
     const response = await fetch(this.url('submitfile'), {
       method: 'POST',
@@ -63,14 +63,6 @@ export default class ConversionClient {
     }
 
     return `${this.baseURL}/${this.id}/${path}`
-  }
-
-  private async assertInputFileExistsWithName(name: string): Promise<void> {
-    const files = await this.getFiles()
-
-    if (files.inputFiles.every(file => file.name !== name)) {
-      throw new Error(`no input file found with name "${name}"`)
-    }
   }
 
   public async reset(): Promise<void> {
